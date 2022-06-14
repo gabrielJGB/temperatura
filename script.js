f358b719af49b7b8611b93d2a34a61417051700a
Gabriel <brunengo.gabriel@gmail.com>
2022-06-14 02:40:55 -0300

    conversion a pwa

---
+++ script.js
@@ -0,0 +1,21 @@
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("service-worker.js");
+}
+
+
+let box = document.querySelector('.box')
+let title = document.querySelector('title')
+fetch('https://ws.smn.gob.ar/map_items/weather')
+    .then((data) => {
+        data.json().then((cities_array) => {
+            cities_array.forEach(city => {
+                if (city.name === 'Bahía Blanca') {
+                    box.textContent = city.weather.tempDesc
+                    title.textContent = "Temp: " + city.weather.tempDesc
+                }
+            })
+        })
+
+    }).catch((e) => {
+        console.log(e)
+    })
